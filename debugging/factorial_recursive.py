0e2a4da31cd9633e58d4377de8973456eb7375d7
Your Name <bezom@hotmail.fr>
2025-05-07 16:58:46 +0200

    modification factorial_recursive.py

--- debugging/factorial_recursive.py
+++ debugging/factorial_recursive.py
@@ -3,19 +3,18 @@ import sys
 
 def factorial(n):
     """
-    Calcule récursivement la factorielle d'un entier n.
+    Calculates the factorial of a given number using recursion.
 
     Parameters:
-    n (int): Un entier non négatif dont on veut calculer la factorielle.
+    n (int): A non-negative integer
 
     Returns:
-    int: La factorielle de n.
+    int: The factorial of the given number
     """
     if n == 0:
         return 1
     else:
         return n * factorial(n - 1)
 
-# Récupère le nombre passé en ligne de commande, calcule sa factorielle et affiche le résultat
 f = factorial(int(sys.argv[1]))
 print(f)
